4787a15d4ff9ccde23703e7638344d1258533904
vividsnow <vividsnow@gmail.com>
2026-03-27 18:09:16 +0300

    v0.19

--- lib/Net/WebSocket/EVx.pm
+++ lib/Net/WebSocket/EVx.pm
@@ -5,18 +5,22 @@ use EV ();
 use XSLoader ();
 our $VERSION;
 BEGIN {
-    $VERSION = '0.17';
+    $VERSION = '0.19';
     XSLoader::load(__PACKAGE__, $VERSION);
 }
-use constant { WS_FRAGMENTED_EOF => 0, WS_FRAGMENTED_ERROR => -1, WS_FRAGMENTED_DATA => 1 };
+use constant { WS_FRAGMENTED_EOF => 0, WS_FRAGMENTED_ERROR => -1, WS_FRAGMENTED_DATA => 1, WS_RSV1_BIT => 0x04 };
 use Exporter 'import';
-our @EXPORT = qw/WS_FRAGMENTED_EOF WS_FRAGMENTED_ERROR WS_FRAGMENTED_NODATA/;
+our @EXPORT = qw/WS_FRAGMENTED_EOF WS_FRAGMENTED_ERROR WS_FRAGMENTED_DATA WS_RSV1_BIT/;
 
 sub new {
     my (undef, $self) = @_;
     $self->{buffering} = 1 unless defined $self->{buffering};
     $self->{type} = 'server' unless defined $self->{type};
-    _wslay_event_context_init($self, ($self->{fd} // fileno($self->{fh})), int($self->{type} eq 'server'));
+    _wslay_event_context_init(
+        $self,
+        (defined($self->{fd}) && $self->{fd} || fileno($self->{fh})),
+        int($self->{type} eq 'server')
+    );
     _wslay_event_config_set_no_buffering($self, int(!$self->{buffering}));
     _wslay_event_config_set_max_recv_msg_length($self, $self->{max_recv_size}) if defined $self->{max_recv_size};
     bless $self
